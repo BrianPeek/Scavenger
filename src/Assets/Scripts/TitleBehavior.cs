c995c5d4457bc5105655403edc0072130735cd95
Brian Peek <brian@brianpeek.com>
2018-04-16 18:08:07 -0400

    Added PlayFab login (requires editor extension + login + setting)

--- src/Assets/Scripts/TitleBehavior.cs
+++ src/Assets/Scripts/TitleBehavior.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,8 +10,28 @@ public class TitleBehavior : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
+		//PlayFabSettings.TitleId = "A246";
+
+#if UNITY_ANDROID
+		LoginWithAndroidDeviceIDRequest req = new LoginWithAndroidDeviceIDRequest
+		{
+			AndroidDeviceId = SystemInfo.deviceUniqueIdentifier,
+			CreateAccount = true
+		};
+		PlayFabClientAPI.LoginWithAndroidDeviceID(req, OnLoginSuccess, OnLoginFailure);
+#endif
 	}
-	
+
+	private void OnLoginSuccess(LoginResult result)
+	{
+		Debug.Log("Logged in");
+	}
+
+	private void OnLoginFailure(PlayFabError error)
+	{
+		Debug.Log("Not logged in - " + error.GenerateErrorReport());
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
