24f22338185e9c14073b6b0b0b00fdedb607a937
Brian Peek <brian@brianpeek.com>
2018-04-15 20:59:34 -0400

    Initial import

---
+++ src/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+	WebCamTexture webCamTexture;
+	public RawImage rawImage;
+	private int last;
+	
+	void Start() 
+	{
+		webCamTexture = new WebCamTexture();
+		rawImage.texture = webCamTexture;
+		rawImage.material.mainTexture = webCamTexture;
+		webCamTexture.Play();
+	}
+
+	private void Update()
+	{
+		
+		if(last != webCamTexture.videoRotationAngle)
+		{
+			rawImage.transform.rotation = Quaternion.AngleAxis(-webCamTexture.videoRotationAngle, Vector3.forward);
+			Debug.Log(webCamTexture.videoRotationAngle);
+		}
+
+		last = webCamTexture.videoRotationAngle;
+	}
+
+	public void StartSnapPhoto()
+	{
+		StartCoroutine(SnapPhoto());
+	}
+
+	private IEnumerator SnapPhoto()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+		photo.SetPixels(webCamTexture.GetPixels());
+		photo.Apply();
+
+		byte[] bytes = photo.EncodeToPNG();
+	}
+}
