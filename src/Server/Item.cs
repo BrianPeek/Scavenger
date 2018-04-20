4896f6a7c1c063b8ee1c3d47f4798c41afe5e730
Brian Peek <brian@brianpeek.com>
2018-04-20 01:29:51 -0400

    just getting some test stuff into github

---
+++ src/Server/Item.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ScavengerServer
+{
+	public class Item
+	{
+		[JsonProperty(PropertyName = "id")]
+		public string Id { get; set; }
+
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+
+		[JsonProperty(PropertyName = "description")]
+		public string Description { get; set; }
+	}
+}
